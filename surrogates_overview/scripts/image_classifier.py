0008b4b153a9cab7186a6dbae2df2aa173edf69b
Kacper Sokol <So-Cool@users.noreply.github.com>
2024-04-26 19:51:09 +0200

    CIFAR models (#6)
    * CIFAR classifiers
    * GitHub Action Ubuntu version bump

--- surrogates_overview/scripts/image_classifier.py
+++ surrogates_overview/scripts/image_classifier.py
@@ -2,8 +2,11 @@
 Image Classifier
 ================
 
-This module implements an image classifier based on PyTorch.
-Inception v3 and AlexNet are availabel.
+This module implements image classifiers based on PyTorch.
+Inception v3 and AlexNet are available for ImageNet;
+ResNet56 is available for CIFAR10; and
+RepVGG (a2) is available for CIFAR100.
+
 See <https://github.com/fat-forensics/resources/tree/master/surrogates_overview>
 for more details.
 """
@@ -11,6 +14,7 @@ for more details.
 # License: new BSD
 
 from scripts.imagenet_label_map import IMAGENET_LABEL_MAP
+from scripts.cifar_label_map import CIFAR10_LABEL_MAP, CIFAR100_LABEL_MAP
 
 import numpy as np
 
@@ -28,6 +32,26 @@ DEVICE = torch.device('cuda:0' if CUDA_AVAILABLE else 'cpu')
 def _get_preprocess_transform():
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transf = transforms.Compose([transforms.ToTensor(), normalize])
+
+    return transf
+
+
+def _get_preprocess_transform_cifar10():
+    # https://github.com/chenyaofo/pytorch-cifar-models/issues/4
+    # https://github.com/chenyaofo/image-classification-codebase/blob/master/conf/cifar10.conf
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
+    transf = transforms.Compose([transforms.ToTensor(), normalize])
+
+    return transf
+
+
+def _get_preprocess_transform_cifar100():
+    # https://github.com/chenyaofo/pytorch-cifar-models/issues/4
+    # https://github.com/chenyaofo/image-classification-codebase/blob/master/conf/cifar100.conf
+    normalize = transforms.Normalize(
+        mean=[0.5070, 0.4865, 0.4409], std=[0.2673, 0.2564, 0.2761])
     transf = transforms.Compose([transforms.ToTensor(), normalize])
 
     return transf
@@ -128,3 +152,77 @@ class ImageClassifier(object):
                 tuples_.append((lab, Y[idx, cls], cls))
             tuples.append(tuples_)
         return tuples
+
+
+class ImageNetClassifier(ImageClassifier):
+    """ImageNet classifiers -- Inception v3 & AlexNet -- based on PyTorch."""
+
+
+class Cifar10Classifier(ImageClassifier):
+    """CIFAR10 classifiers -- ResNet56 -- based on PyTorch."""
+
+    def __init__(self, use_gpu=False):
+        """Initialises the image classifier."""
+        # Get class labels
+        self.class_idx = CIFAR10_LABEL_MAP
+
+        # Get the model
+        # https://github.com/huyvnphan/PyTorch_CIFAR10
+        clf = torch.hub.load(
+            'chenyaofo/pytorch-cifar-models',
+            'cifar10_resnet56',
+            pretrained=True)
+
+        if use_gpu:
+            if CUDA_AVAILABLE:
+                clf = clf.to(DEVICE)
+                # clf.cuda()
+                predict_proba = self._predict_proba_gpu
+            else:
+                logger.warning('GPU was requested but it is not available. '
+                               'Using CPU instead.')
+                predict_proba = self._predict_proba_cpu
+        else:
+            predict_proba = self._predict_proba_cpu
+        self.predict_proba = predict_proba
+
+        self.clf = clf
+        self.clf.eval()
+
+        # Get transformation
+        self.preprocess_transform = _get_preprocess_transform_cifar10()
+
+
+class Cifar100Classifier(ImageClassifier):
+    """CIFAR100 classifiers -- RepVGG (a2) -- based on PyTorch."""
+
+    def __init__(self, use_gpu=False):
+        """Initialises the image classifier."""
+        # Get class labels
+        self.class_idx = CIFAR100_LABEL_MAP
+
+        # Get the model
+        # https://github.com/huyvnphan/PyTorch_CIFAR10
+        clf = torch.hub.load(
+            'chenyaofo/pytorch-cifar-models',
+            'cifar100_repvgg_a2',
+            pretrained=True)
+
+        if use_gpu:
+            if CUDA_AVAILABLE:
+                clf = clf.to(DEVICE)
+                # clf.cuda()
+                predict_proba = self._predict_proba_gpu
+            else:
+                logger.warning('GPU was requested but it is not available. '
+                               'Using CPU instead.')
+                predict_proba = self._predict_proba_cpu
+        else:
+            predict_proba = self._predict_proba_cpu
+        self.predict_proba = predict_proba
+
+        self.clf = clf
+        self.clf.eval()
+
+        # Get transformation
+        self.preprocess_transform = _get_preprocess_transform_cifar100()
